c975ff2b34986f7025372adeb18e2e69f1e2a78d
Diogo Rosário <diogogrosario1008@gmail.com>
2023-04-11 19:27:34 +0100

    Added the deformation scripts, updated the bunny model, setup the finger movement with leapmotion and fixed some bugs

---
+++ Assets/Scripts/FingerMovement.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Leap;
+using Leap.Unity;
+
+public class FingerMovement : MonoBehaviour
+{
+    [SerializeField] private GameObject[] fingerTips;
+    private Vector3[] prevTipsPosition = new Vector3[5];
+    private float[] forcePerFinger = {0f, 0f, 0f, 0f, 0f};
+    [SerializeField] private Vector3[] attemptedPositions = new Vector3[5];
+    private bool handDetected = false;
+    
+    public LeapProvider leapProvider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        for (int i = 0; i < 5; i++)
+        {
+            fingerTips[i].transform.position = attemptedPositions[i];
+            prevTipsPosition[i] = fingerTips[i].transform.position;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        List<Hand> _allHands = Hands.Provider.CurrentFrame.Hands;
+
+        if (_allHands.Count > 0)
+        {
+            if (!handDetected)
+            {
+                handDetected = true;
+                toggleTips(handDetected);
+            }
+            Hand _hand = _allHands[0];
+            Finger _thumb = _hand.GetThumb();
+            Finger _index = _hand.GetIndex();
+            Finger _middle = _hand.GetMiddle();
+            Finger _ring = _hand.GetRing();
+            Finger _pinky = _hand.GetPinky();
+
+            Debug.Log(_thumb.TipPosition);
+            attemptedPositions[0] = _thumb.TipPosition;
+            attemptedPositions[1] = _index.TipPosition;
+            attemptedPositions[2] = _middle.TipPosition;
+            attemptedPositions[3] = _ring.TipPosition;
+            attemptedPositions[4] = _pinky.TipPosition;
+        }
+        else
+        {
+            if (handDetected)
+            {
+                handDetected = false;
+                toggleTips(handDetected);
+            }
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        if (handDetected)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                prevTipsPosition[i] = fingerTips[i].transform.position;
+                Vector3 movementDirection = attemptedPositions[i] - prevTipsPosition[i];
+                Collider bunnyCol = GameObject.Find("stanford-bunny").GetComponent<Collider>();
+                if (bunnyCol.bounds.Contains(attemptedPositions[i]))
+                {
+                    Ray ray = new Ray(prevTipsPosition[i], movementDirection);
+                    RaycastHit hit;
+                    if (Physics.Raycast(ray, out hit))
+                    {
+                        // add an offset to the hit point to make the finger tip stop before getting inside the mesh
+                        hit.point += 0.00001f * hit.normal;
+                        fingerTips[i].transform.position = hit.point;
+                        if (forcePerFinger[i] <= 2.5f) forcePerFinger[i] += 0.3f * movementDirection.magnitude;
+                        MeshDeformer deformer = hit.collider.GetComponent<MeshDeformer>();
+                        if (deformer != null)
+                        {
+                            //Debug.Log("force: " + forcePerFinger[i]);
+                            //Debug.Log("hit point: " + hit.point);
+                            deformer.AddDeformingForce(hit.point, forcePerFinger[i]);
+                        }
+                    }
+                }
+                else
+                {
+                    fingerTips[i].transform.position = attemptedPositions[i];
+                    forcePerFinger[i] = 0f;
+                }
+            }
+        }
+    }
+
+    void toggleTips(bool state)
+    {
+        foreach (var tip in fingerTips)
+        {
+            tip.SetActive(state);
+        }
+    }
+}
---
+++ Assets/Scripts/MeshDeformer.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class MeshDeformer : MonoBehaviour {
+
+	public float springForce = 20f;
+	public float damping = 5f;
+
+	Mesh deformingMesh;
+	MeshFilter meshFilter;
+	Vector3[] originalVertices, displacedVertices;
+	Vector3[] vertexVelocities;
+	[SerializeField] MeshCollider col;
+	float uniformScale = 1f;
+	bool alreadyReset = false;
+	bool changed = false;
+	int colliderUpdateTreshold = 20;
+	Vector3 lastHitPoint = Vector3.zero;
+
+	void Start () {
+		if(damping <= 0) damping = 1;
+		if(springForce <= 0) springForce = 1;
+		meshFilter = GetComponent<MeshFilter>();
+		deformingMesh = meshFilter.mesh;
+		originalVertices = deformingMesh.vertices;
+		displacedVertices = new Vector3[originalVertices.Length];
+		for (int i = 0; i < originalVertices.Length; i++) {
+			displacedVertices[i] = originalVertices[i];
+		}
+		vertexVelocities = new Vector3[originalVertices.Length];
+	}
+
+
+	void FixedUpdate () {
+		int changedVertexCount = 0;
+		uniformScale = transform.localScale.x;
+		if(!alreadyReset && !changed){
+			deformingMesh.vertices = originalVertices;
+			deformingMesh.RecalculateNormals();
+			col.sharedMesh = deformingMesh;
+			//Debug.Log("Reseted collider");
+			alreadyReset = true;
+		}
+		for (int i = 0; i < displacedVertices.Length; i++) {
+			UpdateVertex(i);
+			if(Vector3.Distance(displacedVertices[i], originalVertices[i]) >= 0.001f){
+				changedVertexCount++;
+			}
+		}
+		if(changedVertexCount >= 0.20f*displacedVertices.Length){
+			deformingMesh.vertices = displacedVertices;
+			deformingMesh.RecalculateNormals();
+			if(colliderUpdateTreshold > 0){
+				colliderUpdateTreshold--;
+				changedVertexCount = 0;
+				return;
+			}
+			colliderUpdateTreshold = 20;
+			col.sharedMesh = deformingMesh;
+			//Debug.Log("Changed collider");
+			changed = true;
+			alreadyReset = false;
+		}
+		else{
+			changed = false;
+		}
+		changedVertexCount = 0;
+	}
+
+	void UpdateVertex (int i) {
+		Vector3 velocity = vertexVelocities[i];
+		Vector3 displacement = displacedVertices[i] - originalVertices[i];
+		displacement *= uniformScale;
+		velocity -= displacement * springForce * Time.deltaTime;
+		float resistence = damping * Time.deltaTime;
+		if(resistence > 1f) resistence = 1f;
+		velocity *= 1f - resistence;
+		vertexVelocities[i] = velocity;
+		displacedVertices[i] += velocity * (Time.deltaTime / uniformScale);
+	}
+
+	public void AddDeformingForce (Vector3 point, float force) {
+		point = transform.InverseTransformPoint(point);
+		//Debug.Log("After inverse transform: " + point);
+		for (int i = 0; i < displacedVertices.Length; i++) {
+			AddForceToVertex(i, point, force);
+		}
+	}
+
+	void AddForceToVertex (int i, Vector3 point, float force) {
+		Vector3 pointToVertex = displacedVertices[i] - point;
+		pointToVertex *= uniformScale;
+		float attenuatedForce = -force / (1f + pointToVertex.sqrMagnitude);
+		if(Mathf.Abs(attenuatedForce) < 0.01f)
+			attenuatedForce = 0;
+		float velocity = attenuatedForce * Time.deltaTime;
+		vertexVelocities[i] += pointToVertex.normalized * velocity;
+	}
+}
